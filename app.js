f34bc92c72685b7661891e0a190e3caed8615aad
Sam Adams <sam.adams7785@gmail.com>
2022-09-07 13:00:38 -0700

    added input box and function

--- app.js
+++ app.js
@@ -2,7 +2,17 @@
 
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
+
+const nameInput = document.getElementById('name-input');
+const changeButton = document.getElementById('change-name');
+const nameDisplay = document.getElementById('name-display');
+
+changeButton.addEventListener('click', () => {
+    const name = nameInput.value;
+    nameDisplay.textContent = name;
+    nameInput.value = '';
+});
